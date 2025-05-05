7f7c1784d51a4794ead98658bac3c818a5cd27ef
Santucho12 <santysegal@gmail.com>
2025-05-05 01:02:25 -0300

    solucionando errores

--- repo individual/PARCIAL-SEGAL/ejercicio-2/ejercicio2.js
+++
@@ -1,9 +0,0 @@
-const imgvista = document.querySelector("img");
-const galeria =document.querySelector("#galeria");
-
-
-galeria.addEventListener("click", mostrarImagen);
-function mostrarImagen(evento){
-        imgvista.src = evento.target.src;
-    }
-    
--- repo individual/TP2/readme.md
+++
@@ -1,1 +0,0 @@
-# practico 2
