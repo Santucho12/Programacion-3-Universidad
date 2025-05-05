f2389540e8f3df8d5997535c41a315ba11064ee1
Santucho12 <santysegal@gmail.com>
2025-05-05 01:00:49 -0300

    solucionando problemas con el repo

---
+++ node/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { multiplicar } = require('./matematica');
+
+// Inicializar la app
+const app = express();
+
+// Configurar el puerto
+const PORT = 3000;
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Ruta básica
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+
+// Mostrar el resultado de la multiplicación
+console.log(multiplicar(6, 52));
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
