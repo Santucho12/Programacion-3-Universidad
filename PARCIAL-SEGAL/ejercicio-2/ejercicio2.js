f1f9cc70c2583c4f3f92caeaf492bb591d2dd4dc
Santucho12 <santysegal@gmail.com>
2025-05-05 01:35:33 -0300

    errores solucionados

--- PARCIAL-SEGAL/ejercicio-2/ejercicio2.js
+++ PARCIAL-SEGAL/ejercicio-2/ejercicio2.js
@@ -2,7 +2,7 @@ const imgvista = document.querySelector("img");
 const galeria =document.querySelector("#galeria");
 
 
-galeria.addEventListener("click", mostrarImagen);
+galeria.addEventListener("click", mostrarImagen );
 function mostrarImagen(evento){
         imgvista.src = evento.target.src ;
     }
